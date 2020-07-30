b7d10e4d39a2c908e6888017b284d2ceeacbd0f7
xthemadgenius <javierjcjr@gmail.com>
2020-07-30 01:46:49 -0700

    got User Dashboard Working

--- client/src/components/ops/Users.js
+++ client/src/components/ops/Users.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import DashHead from '../select/DashHead';
 import {Link} from '@reach/router';
 import './ops.css';
+import axios from 'axios';
 
 const Users = (props) => {
+
+    const [user, setUser] = useState([]);
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/users`)
+        .then(res => setUser(res.data))
+        .catch(err => console.log(err));
+    })
 
     return (
         <div className="theTable">
@@ -15,56 +24,15 @@ const Users = (props) => {
                     <th>Skills</th>
                     <th>Readiness</th>
                 </tr>
-                <tr>
-                    <td><Link to="/users/1">Alfreds Futterkiste</Link></td>
-                    <td>React, Javascript, Html, css, Python, Node.js, AWS</td>
-                    <td>Ready</td>
-                </tr>
-                <tr>
-                    <td><Link to="/users/1">Alfreds Futterkiste</Link></td>
-                    <td>React, Javascript, Html, css, Python, Node.js, AWS</td>
-                    <td>Ready</td>
-                </tr>
-                <tr>
-                    <td><Link to="/users/1">Alfreds Futterkiste</Link></td>
-                    <td>React, Javascript, Html, css, Python, Node.js, AWS</td>
-                    <td>Ready</td>
-                </tr>
-                <tr>
-                    <td><Link to="/users/1">Alfreds Futterkiste</Link></td>
-                    <td>React, Javascript, Html, css, Python, Node.js, AWS</td>
-                    <td>Ready</td>
-                </tr>
-                <tr>
-                    <td><Link to="/users/1">Alfreds Futterkiste</Link></td>
-                    <td>React, Javascript, Html, css, Python, Node.js, AWS</td>
-                    <td>Ready</td>
-                </tr>
-                <tr>
-                    <td><Link to="/users/1">Alfreds Futterkiste</Link></td>
-                    <td>React, Javascript, Html, css, Python, Node.js, AWS</td>
-                    <td>Ready</td>
-                </tr>
-                <tr>
-                    <td><Link to="/users/1">Alfreds Futterkiste</Link></td>
-                    <td>React, Javascript, Html, css, Python, Node.js, AWS</td>
-                    <td>Ready</td>
-                </tr>
-                <tr>
-                    <td><Link to="/users/1">Alfreds Futterkiste</Link></td>
-                    <td>React, Javascript, Html, css, Python, Node.js, AWS</td>
-                    <td>Ready</td>
-                </tr>
-                <tr>
-                    <td><Link to="/users/1">Alfreds Futterkiste</Link></td>
-                    <td>React, Javascript, Html, css, Python, Node.js, AWS</td>
-                    <td>Ready</td>
-                </tr>
-                <tr>
-                    <td><Link to="/users/1">Alfreds Futterkiste</Link></td>
-                    <td>React, Javascript, Html, css, Python, Node.js, AWS</td>
-                    <td>Ready</td>
-                </tr>
+                {user.map((users,idx) => {
+                    return(
+                        <tr key={idx}>
+                            <td><Link to={`/users/${users._id}`}>{users.firstName} {users.lastName}</Link></td>
+                            <td>React, Javascript, Html, CSS, Python, Node.js, AWS</td>
+                            <td>Ready</td>
+                        </tr>
+                    )
+                })}
             </table>
         </div>
     )

723d50142870261ff08cc34a40d8fa10f9c801ce
xthemadgenius <javierjcjr@gmail.com>
2020-07-30 11:53:39 -0700

    profile form has passable data

--- client/src/views/profile/Profile.js
+++ client/src/views/profile/Profile.js
@@ -6,16 +6,18 @@ import axios from 'axios';
 
 const Profile = (props) => {
 
-    const [user, setUser] =useState({});
+    const {id} = props;
+    const [users, setUsers] =useState({});
 
     useEffect(() => {
-        axios.get(`http:localhost:8000/api/users/${props.id}`)
-        .then(res => setUser(res.data))
+        axios.get(`http://localhost:8000/api/users/${id}`)
+        .then(res => setUsers(res.data))
+        .catch((err) => console.log(err));
     }, [])
 
     return (
         <div className="profile">
-            <h1>Naruto Uzamaki</h1>
+            <h1>{users.firstName} {users.lastName} </h1>
             <img src="https://i.pinimg.com/originals/2f/59/f1/2f59f17586bc76ec69071cd3672cdc4d.png" alt=""/>
             <h3>Skills</h3>
             <p>React, Javascript, Html, css, Python, Node.js, AWS</p>
@@ -29,7 +31,7 @@ const Profile = (props) => {
             <Link className="troll" to="/collaborate/1">Rhinos Point</Link>
             <h3>Collaboration Contributions</h3>
             <Link className="troll" to="/collaborate/1">Rhinos Point</Link><br/><br/><br/>
-            <Link className="bro" to="/users/1/edit">Edit</Link><br/><br/><br/>
+            <Link className="bro" to={`/users/${users._id}/edit`}>Edit</Link><br/><br/><br/>
             <Link className="bro" to="/users/1/create/collab">Create Collab</Link><br/><br/><br/>
             <Link className="bro" to="/users/1/create/job">Create Job</Link>
             <ReviewForm/>
